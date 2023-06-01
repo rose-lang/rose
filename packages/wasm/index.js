65eb516bdfb4d8ebd333bc644d843552f700882b
Raven Rothkopf <rgr2124@barnard.edu>
2023-06-01 15:05:36 -0400

    oops not allowed!

--- packages/wasm/index.js
+++ packages/wasm/index.js
@@ -1,8 +1,7 @@
-import { initialize } from "@rose-lang/wasm";
-import init from "./wbg/rose_web.js";
+import init, { initialize } from "./wbg/rose_web.js";
 import bytes from "./wbg/rose_web_bg.wasm";
 
-initialize();
 await init(bytes);
+initialize();
 
 export * from "./wbg/rose_web.js";
