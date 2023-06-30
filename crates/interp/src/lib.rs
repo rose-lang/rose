cdc9e8379a275d79fe2d2c0c2e8d553b98f979d0
Sam Estep <sam@samestep.com>
2023-06-30 18:30:40 -0400

    Start fixing interpreter

--- crates/interp/src/lib.rs
+++ crates/interp/src/lib.rs
@@ -1,4 +1,4 @@
-use rose::{id, Binop, Def, Function, Instr, Size, Type, Unop};
+use rose::{id, Binop, Block, Expr, Func, Function, Instr, Type, Typexpr, Unop};
 use std::rc::Rc;
 
 #[cfg(feature = "serde")]
@@ -11,8 +11,8 @@ use ts_rs::TS;
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Clone, Debug, PartialEq)]
 pub enum Val {
+    Unit,
     Bool(bool),
-    I32(i32),
     F64(f64),
     Tuple(Rc<Vec<Val>>),
     Vector(Rc<Vec<Val>>),
@@ -20,9 +20,9 @@ pub enum Val {
 
 fn type_of(x: &Val) -> Type {
     match x {
+        Val::Unit => Type::Unit,
         Val::Bool(_) => Type::Bool,
-        Val::I32(_) => Type::Int,
-        Val::F64(_) => Type::Real,
+        Val::F64(_) => Type::F64,
         Val::Tuple(_) => todo!(),
         Val::Vector(_) => todo!(),
     }
@@ -43,13 +43,13 @@ pub enum Error {
     BadGeneric { num: usize, id: id::Generic },
 
     #[error("have {num} locals, {id:?} out of range")]
-    BadLocal { num: usize, id: id::Local },
+    BadLocal { num: usize, id: id::Var },
 
     #[error("have {num} function references, {id:?} out of range")]
     BadFunc { num: usize, id: id::Func },
 
     #[error("local {id:?} is unset")]
-    UnsetLocal { id: id::Local },
+    UnsetLocal { id: id::Var },
 
     #[error("expected primitive type {expected:?}, got {actual:?}")]
     ExpectedPrimitive { expected: Type, actual: Type },
@@ -72,10 +72,9 @@ fn type_error(t: Type, x: &Val) -> Error {
 }
 
 struct Interpreter<'a> {
-    f: &'a Def<Function>,
+    f: &'a Function,
     generics: &'a [usize],
-    locals: Vec<Option<Val>>,
-    stack: Vec<Val>,
+    vars: Vec<Option<Val>>,
 }
 
 impl<'a> Interpreter<'a> {
@@ -90,17 +89,10 @@ impl<'a> Interpreter<'a> {
         }
     }
 
-    fn pop_i32(&mut self) -> Result<i32, Error> {
-        match self.pop()? {
-            Val::I32(x) => Ok(x),
-            x => Err(type_error(Type::Int, &x)),
-        }
-    }
-
     fn pop_f64(&mut self) -> Result<f64, Error> {
         match self.pop()? {
             Val::F64(x) => Ok(x),
-            x => Err(type_error(Type::Int, &x)),
+            x => Err(type_error(Type::F64, &x)),
         }
     }
 
@@ -110,73 +102,19 @@ impl<'a> Interpreter<'a> {
         Ok(())
     }
 
-    fn unary_i32(&mut self, f: impl Fn(i32) -> i32) -> Result<(), Error> {
-        let a = self.pop_i32()?;
-        self.stack.push(Val::I32(f(a)));
-        Ok(())
-    }
-
     fn unary_f64(&mut self, f: impl Fn(f64) -> f64) -> Result<(), Error> {
         let a = self.pop_f64()?;
         self.stack.push(Val::F64(f(a)));
         Ok(())
     }
 
-    fn fold_i32(&mut self, init: i32, f: impl Fn(i32, i32) -> i32) -> Result<(), Error> {
-        match self.pop()? {
-            Val::Vector(v) => {
-                let mut acc = init;
-                for val in v.as_ref() {
-                    match val {
-                        Val::I32(x) => acc = f(acc, *x),
-                        x => return Err(type_error(Type::Int, x)),
-                    }
-                }
-                self.stack.push(Val::I32(acc));
-                Ok(())
-            }
-            x => Err(Error::ExpectedVector {
-                actual: type_of(&x),
-            }),
-        }
-    }
-
-    fn fold_f64(&mut self, init: f64, f: impl Fn(f64, f64) -> f64) -> Result<(), Error> {
-        match self.pop()? {
-            Val::Vector(v) => {
-                let mut acc = init;
-                for val in v.as_ref() {
-                    match val {
-                        Val::F64(x) => acc = f(acc, *x),
-                        x => return Err(type_error(Type::Real, x)),
-                    }
-                }
-                self.stack.push(Val::F64(acc));
-                Ok(())
-            }
-            x => Err(Error::ExpectedVector {
-                actual: type_of(&x),
-            }),
-        }
-    }
-
     fn unary(&mut self, op: Unop) -> Result<(), Error> {
         use Unop::*;
         match op {
             Not => self.unary_bool(|a| !a),
-            NegInt => self.unary_i32(|a| -a),
-            AbsInt => self.unary_i32(|a| a.abs()),
             NegReal => self.unary_f64(|a| -a),
             AbsReal => self.unary_f64(|a| a.abs()),
             Sqrt => self.unary_f64(|a| a.sqrt()),
-            SumInt => self.fold_i32(0, |a, b| a + b),
-            ProdInt => self.fold_i32(1, |a, b| a * b),
-            MaxInt => self.fold_i32(i32::MIN, |a, b| a.max(b)),
-            MinInt => self.fold_i32(i32::MAX, |a, b| a.min(b)),
-            SumReal => self.fold_f64(0., |a, b| a + b),
-            ProdReal => self.fold_f64(1., |a, b| a * b),
-            MaxReal => self.fold_f64(f64::NEG_INFINITY, |a, b| a.max(b)),
-            MinReal => self.fold_f64(f64::INFINITY, |a, b| a.min(b)),
         }
     }
 
@@ -187,24 +125,10 @@ impl<'a> Interpreter<'a> {
         Ok(())
     }
 
-    fn comp_i32(&mut self, f: impl Fn(i32, i32) -> bool) -> Result<(), Error> {
-        let b = self.pop_i32()?;
-        let a = self.pop_i32()?;
-        self.stack.push(Val::Bool(f(a, b)));
-        Ok(())
-    }
-
     fn comp_f64(&mut self, f: impl Fn(f64, f64) -> bool) -> Result<(), Error> {
         let b = self.pop_f64()?;
         let a = self.pop_f64()?;
         self.stack.push(Val::Bool(f(a, b)));
-        Ok(())
-    }
-
-    fn bin_i32(&mut self, f: impl Fn(i32, i32) -> i32) -> Result<(), Error> {
-        let b = self.pop_i32()?;
-        let a = self.pop_i32()?;
-        self.stack.push(Val::I32(f(a, b)));
         Ok(())
     }
 
@@ -222,23 +146,12 @@ impl<'a> Interpreter<'a> {
             Or => self.logic(|a, b| a || b),
             EqBool => self.logic(|a, b| a == b),
             NeqBool => self.logic(|a, b| a != b),
-            NeqInt => self.comp_i32(|a, b| a != b),
-            LtInt => self.comp_i32(|a, b| a < b),
-            LeqInt => self.comp_i32(|a, b| a <= b),
-            EqInt => self.comp_i32(|a, b| a == b),
-            GtInt => self.comp_i32(|a, b| a > b),
-            GeqInt => self.comp_i32(|a, b| a >= b),
             NeqReal => self.comp_f64(|a, b| a != b),
             LtReal => self.comp_f64(|a, b| a < b),
             LeqReal => self.comp_f64(|a, b| a <= b),
             EqReal => self.comp_f64(|a, b| a == b),
             GtReal => self.comp_f64(|a, b| a > b),
             GeqReal => self.comp_f64(|a, b| a >= b),
-            AddInt => self.bin_i32(|a, b| a + b), // TODO: handle overflow
-            SubInt => self.bin_i32(|a, b| a - b),
-            MulInt => self.bin_i32(|a, b| a * b),
-            DivInt => self.bin_i32(|a, b| a / b),
-            Mod => self.bin_i32(|a, b| a % b),
             AddReal => self.bin_f64(|a, b| a + b),
             SubReal => self.bin_f64(|a, b| a - b),
             MulReal => self.bin_f64(|a, b| a * b),
@@ -246,19 +159,9 @@ impl<'a> Interpreter<'a> {
         }
     }
 
-    fn get_generic(&self, id: id::Generic) -> Result<usize, Error> {
-        self.generics
-            .get(id.generic())
-            .copied()
-            .ok_or(Error::BadGeneric {
-                num: self.f.generics,
-                id,
-            })
-    }
-
-    fn instr(&mut self, instr: Instr) -> Result<(), Error> {
-        use Instr::*;
-        match instr {
+    fn instr(&mut self, instr: &Instr) -> Result<(), Error> {
+        use Expr::*;
+        match instr.expr {
             Generic { id } => {
                 let val = self.get_generic(id)?;
                 self.stack.push(Val::I32(
@@ -334,7 +237,7 @@ impl<'a> Interpreter<'a> {
 }
 
 // TODO: return a stack trace with instruction indices, instead of just the error kind
-pub fn interp(f: &Def<Function>, generics: &[usize], args: Vec<Val>) -> Result<Vec<Val>, Error> {
+pub fn interp(f: &Function, generics: &[usize], args: Vec<Val>) -> Result<Vec<Val>, Error> {
     if generics.len() != f.generics {
         return Err(Error::WrongGenerics {
             expected: f.generics,
@@ -362,20 +265,52 @@ pub fn interp(f: &Def<Function>, generics: &[usize], args: Vec<Val>) -> Result<V
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rose::{Inst, Typexpr};
 
     #[test]
     fn test_two_plus_two() {
-        let f = Def {
-            generics: 0,
-            types: vec![],
-            def: Function {
-                params: vec![Type::Real, Type::Real],
-                ret: vec![Type::Real],
-                locals: vec![],
-                funcs: vec![],
-                body: vec![Instr::Binary { op: Binop::AddReal }],
-            },
+        let f = Function {
+            generics: vec![],
+            types: vec![Typexpr::Tuple {
+                members: vec![Type::F64, Type::F64],
+            }],
+            funcs: vec![],
+            param: Type::Expr { id: id::typexpr(0) },
+            ret: Type::F64,
+            vars: vec![
+                Type::Expr { id: id::typexpr(0) },
+                Type::F64,
+                Type::F64,
+                Type::F64,
+            ],
+            blocks: vec![Block {
+                arg: id::var(0),
+                code: vec![
+                    Instr {
+                        var: id::var(1),
+                        expr: Expr::Member {
+                            tuple: id::var(0),
+                            member: id::member(0),
+                        },
+                    },
+                    Instr {
+                        var: id::var(2),
+                        expr: Expr::Member {
+                            tuple: id::var(0),
+                            member: id::member(1),
+                        },
+                    },
+                    Instr {
+                        var: id::var(3),
+                        expr: Expr::Binary {
+                            op: Binop::Add,
+                            left: id::var(1),
+                            right: id::var(2),
+                        },
+                    },
+                ],
+                ret: id::var(3),
+            }],
+            main: id::block(0),
         };
         let answer = interp(&f, &[], vec![Val::F64(2.), Val::F64(2.)]).unwrap();
         assert_eq!(answer, vec![Val::F64(4.)]);
@@ -383,96 +318,57 @@ mod tests {
 
     #[test]
     fn test_nested_call() {
-        let f = Rc::new(Def {
-            generics: 0,
+        let f = Rc::new(Function {
+            generics: vec![],
             types: vec![],
-            def: Function {
-                params: vec![],
-                ret: vec![Type::Real],
-                locals: vec![],
-                funcs: vec![],
-                body: vec![Instr::Real { val: 42. }],
-            },
-        });
-        let g = Def {
-            generics: 0,
-            types: vec![],
-            def: Function {
-                params: vec![],
-                ret: vec![Type::Real],
-                locals: vec![Type::Real],
-                funcs: vec![Inst {
-                    def: f,
-                    params: vec![],
+            funcs: vec![],
+            param: Type::Unit,
+            ret: Type::F64,
+            vars: vec![Type::Unit, Type::F64],
+            blocks: vec![Block {
+                arg: id::var(0),
+                code: vec![Instr {
+                    var: id::var(1),
+                    expr: Expr::F64 { val: 42. },
                 }],
-                body: vec![
-                    Instr::Call { id: id::func(0) },
-                    Instr::Set { id: id::local(0) },
-                    Instr::Get { id: id::local(0) },
-                    Instr::Get { id: id::local(0) },
-                    Instr::Binary { op: Binop::MulReal },
+                ret: id::var(1),
+            }],
+            main: id::block(0),
+        });
+        let g = Function {
+            generics: vec![],
+            types: vec![],
+            funcs: vec![Func {
+                def: f,
+                generics: vec![],
+            }],
+            param: Type::Unit,
+            ret: Type::F64,
+            vars: vec![Type::Unit, Type::F64, Type::F64],
+            blocks: vec![Block {
+                arg: id::var(0),
+                code: vec![
+                    Instr {
+                        var: id::var(1),
+                        expr: Expr::Call {
+                            func: id::func(0),
+                            arg: id::var(0),
+                        },
+                    },
+                    Instr {
+                        var: id::var(2),
+                        expr: Expr::Binary {
+                            op: Binop::Mul,
+                            left: id::var(1),
+                            right: id::var(1),
+                        },
+                    },
                 ],
-            },
+                ret: id::var(2),
+            }],
+            main: id::block(0),
         };
         let answer = interp(&g, &[], vec![]).unwrap();
         assert_eq!(answer, vec![Val::F64(1764.)]);
-    }
-
-    #[test]
-    fn test_sum_int() {
-        let f = Def {
-            generics: 1,
-            types: vec![Typexpr::Vector {
-                elem: Type::Int,
-                size: Size::Generic { id: id::generic(0) },
-            }],
-            def: Function {
-                params: vec![Type::Var { id: id::typ(0) }],
-                ret: vec![Type::Int],
-                locals: vec![],
-                funcs: vec![],
-                body: vec![Instr::Unary { op: Unop::SumInt }],
-            },
-        };
-        let answer = interp(
-            &f,
-            &[3],
-            vec![Val::Vector(Rc::new(vec![
-                Val::I32(1),
-                Val::I32(2),
-                Val::I32(3),
-            ]))],
-        )
-        .unwrap();
-        assert_eq!(answer, vec![Val::I32(6)]);
-    }
-
-    #[test]
-    fn test_prod_real() {
-        let f = Def {
-            generics: 1,
-            types: vec![Typexpr::Vector {
-                elem: Type::Real,
-                size: Size::Generic { id: id::generic(0) },
-            }],
-            def: Function {
-                params: vec![Type::Var { id: id::typ(0) }],
-                ret: vec![Type::Real],
-                locals: vec![],
-                funcs: vec![],
-                body: vec![Instr::Unary { op: Unop::ProdReal }],
-            },
-        };
-        let answer = interp(
-            &f,
-            &[3],
-            vec![Val::Vector(Rc::new(vec![
-                Val::F64(2.),
-                Val::F64(3.),
-                Val::F64(4.),
-            ]))],
-        )
-        .unwrap();
-        assert_eq!(answer, vec![Val::F64(24.)]);
     }
 }
