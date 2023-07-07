ad55bb5b503db59f205836e47b8ea18b617eba7c
Sam Estep <sam@samestep.com>
2023-07-06 22:44:10 -0400

    Write a pprint function

--- packages/core/src/debug.test.ts
+++ packages/core/src/debug.test.ts
@@ -1,6 +1,6 @@
 import * as wasm from "@rose-lang/wasm";
 import { expect, test } from "vitest";
-import { Real, add, fn, mul } from "./index.js";
+import { Real, add, cond, fn, lt, mul, sub } from "./index.js";
 
 test("core IR type layouts", () => {
   // these don't matter too much, but it's good to notice if sizes increase
@@ -11,131 +11,49 @@ test("core IR type layouts", () => {
   });
 });
 
-test("test rose to rust formatting", () => {
-  const f = fn([Real, Real], Real, (x, y) => add(mul(x, 2), y));
-  const g = wasm.js2Rust(f.f.f);
-  // TODO: maybe make a more readable IR text format
-  expect(g).toBe(
-    `Function {
-    generics: [],
-    types: [
-        Bool,
-        F64,
-        Tuple {
-            members: [
-                Ty(
-                    1,
-                ),
-                Ty(
-                    1,
-                ),
-            ],
-        },
-    ],
-    funcs: [],
-    param: Ty(
-        2,
-    ),
-    ret: Ty(
-        1,
-    ),
-    vars: [
-        Ty(
-            2,
-        ),
-        Ty(
-            1,
-        ),
-        Ty(
-            1,
-        ),
-        Ty(
-            1,
-        ),
-        Ty(
-            1,
-        ),
-        Ty(
-            1,
-        ),
-    ],
-    blocks: [
-        Block {
-            arg: Var(
-                0,
-            ),
-            code: [
-                Instr {
-                    var: Var(
-                        1,
-                    ),
-                    expr: Member {
-                        tuple: Var(
-                            0,
-                        ),
-                        member: Member(
-                            0,
-                        ),
-                    },
-                },
-                Instr {
-                    var: Var(
-                        2,
-                    ),
-                    expr: Member {
-                        tuple: Var(
-                            0,
-                        ),
-                        member: Member(
-                            1,
-                        ),
-                    },
-                },
-                Instr {
-                    var: Var(
-                        3,
-                    ),
-                    expr: F64 {
-                        val: 2.0,
-                    },
-                },
-                Instr {
-                    var: Var(
-                        4,
-                    ),
-                    expr: Binary {
-                        op: Mul,
-                        left: Var(
-                            1,
-                        ),
-                        right: Var(
-                            3,
-                        ),
-                    },
-                },
-                Instr {
-                    var: Var(
-                        5,
-                    ),
-                    expr: Binary {
-                        op: Add,
-                        left: Var(
-                            4,
-                        ),
-                        right: Var(
-                            2,
-                        ),
-                    },
-                },
-            ],
-            ret: Var(
-                5,
-            ),
-        },
-    ],
-    main: Block(
-        0,
-    ),
-}`
+test("pprint", () => {
+  const f = fn([Real, Real], Real, (x, y) => {
+    const p = lt(x, y);
+    const z = cond(
+      p,
+      () => {
+        const a = mul(x, y);
+        return add(a, x);
+      },
+      () => {
+        const b = sub(y, x);
+        return mul(b, y);
+      }
+    );
+    const w = add(z, x);
+    return add(y, w);
+  });
+  const s = wasm.pprint(f.f.f);
+  expect(s).toBe(
+    `
+T0 = Bool
+T1 = F64
+T2 = (T1, T1)
+T3 = Unit
+x0: T2 -> T1 {
+  x1: T1 = x0.0
+  x2: T1 = x0.1
+  x3: T0 = x1 < x2
+  x10: T1 = if x3 {
+    x4: T3
+    x5: T1 = x1 * x2
+    x6: T1 = x5 + x1
+    x6
+  } else {
+    x7: T3
+    x8: T1 = x2 - x1
+    x9: T1 = x8 * x2
+    x9
+  }
+  x11: T1 = x10 + x1
+  x12: T1 = x2 + x11
+  x12
+}
+`.trimStart()
   );
 });
