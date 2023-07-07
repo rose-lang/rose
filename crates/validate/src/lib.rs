84724e606c8f0c963d310056ab99ebbeb269b249
Sam Estep <sam@samestep.com>
2023-07-07 12:47:00 -0400

    Use `Ty` to simplify

--- crates/validate/src/lib.rs
+++ crates/validate/src/lib.rs
@@ -1,81 +1,41 @@
-use rose::{id, FuncNode, Function, Type, Typexpr};
-
-#[derive(Debug, thiserror::Error)]
-pub enum TypeError {
-    #[error("generic ID {} is out of range", .id.generic())]
-    InvalidGeneric { id: id::Generic },
-
-    #[error("scope ID {} is out of range", .id.block())]
-    InvalidScope { id: id::Block },
-
-    #[error("type ID {} is out of range", .id.typexpr())]
-    InvalidType { id: id::Typexpr },
-}
-
-struct FuncValidator<'a> {
-    f: &'a Function,
-}
-
-impl FuncValidator<'_> {
-    fn ty(&self, t: Type) -> Result<(), TypeError> {
-        match t {
-            Type::Unit | Type::Bool | Type::F64 | Type::Fin { .. } => Ok(()),
-            Type::Generic { id } => {
-                if id.generic() < self.f.generics.len() {
-                    Ok(())
-                } else {
-                    Err(TypeError::InvalidGeneric { id })
-                }
-            }
-            Type::Scope { id } => {
-                if id.block() < self.f.blocks.len() {
-                    Ok(())
-                } else {
-                    Err(TypeError::InvalidScope { id })
-                }
-            }
-            Type::Expr { id } => {
-                if id.typexpr() < self.f.types.len() {
-                    Ok(())
-                } else {
-                    Err(TypeError::InvalidType { id })
-                }
-            }
-        }
-    }
-}
+use rose::{id, FuncNode, Ty};
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("constraints for generic {} are incompatible", .0.generic())]
     IncompatibleConstraints(id::Generic),
 
-    #[error("scope for type {} is invalid", .0.typexpr())]
-    InvalidScope(id::Typexpr, #[source] TypeError),
+    #[error("generic ID for type {} is out of range", .0.ty())]
+    InvalidGeneric(id::Ty),
 
-    #[error("scope for type {} is not a scope", .0.typexpr())]
-    NotScope(id::Typexpr),
+    #[error("block ID for type {} is out of range", .0.ty())]
+    InvalidBlock(id::Ty),
 
-    #[error("inner for type {} is invalid", .0.typexpr())]
-    InvalidInner(id::Typexpr, #[source] TypeError),
+    #[error("scope type ID for type {} is out of range", .0.ty())]
+    InvalidScope(id::Ty),
 
-    #[error("index for type {} is invalid", .0.typexpr())]
-    InvalidIndex(id::Typexpr, #[source] TypeError),
+    #[error("scope for type {} is not a scope", .0.ty())]
+    NotScope(id::Ty),
 
-    #[error("index for type {} is not an index", .0.typexpr())]
-    NotIndex(id::Typexpr),
+    #[error("inner type ID for type {} is out of range", .0.ty())]
+    InvalidInner(id::Ty),
 
-    #[error("element for type {} is invalid", .0.typexpr())]
-    InvalidElem(id::Typexpr, #[source] TypeError),
+    #[error("index type ID for type {} is out of range", .0.ty())]
+    InvalidIndex(id::Ty),
 
-    #[error("member {} for type {} is invalid", .1.member(), .0.typexpr())]
-    InvalidMember(id::Typexpr, id::Member, #[source] TypeError),
+    #[error("index for type {} is not an index", .0.ty())]
+    NotIndex(id::Ty),
+
+    #[error("element type ID for type {} is out of range", .0.ty())]
+    InvalidElem(id::Ty),
+
+    #[error("member {} type ID for type {} is out of range", .1.member(), .0.ty())]
+    InvalidMember(id::Ty, id::Member),
 }
 
 /// Validate `f`, assuming that all of its referenced types and functions are valid.
 pub fn validate_func(f: impl FuncNode) -> Result<(), Error> {
     let def = f.def();
-    let validate = FuncValidator { f: def };
 
     for (i, constraints) in def.generics.iter().enumerate() {
         // for now we have no compatible constraints
@@ -84,32 +44,54 @@ pub fn validate_func(f: impl FuncNode) -> Result<(), Error> {
         }
     }
 
-    for (i, typexpr) in def.types.iter().enumerate() {
-        let id = id::typexpr(i);
-        match typexpr {
-            &Typexpr::Ref { scope, inner } => {
-                validate.ty(scope).map_err(|e| Error::InvalidScope(id, e))?;
-                validate.ty(inner).map_err(|e| Error::InvalidInner(id, e))?;
-                if !matches!(scope, Type::Scope { .. }) {
-                    return Err(Error::NotScope(id));
+    for (i, ty) in def.types.iter().enumerate() {
+        let t = id::ty(i);
+        match ty {
+            Ty::Unit | Ty::Bool | Ty::F64 | Ty::Fin { .. } => Ok(()),
+            Ty::Generic { id } => {
+                if id.generic() >= def.generics.len() {
+                    Err(Error::InvalidGeneric(t))
+                } else {
+                    Ok(())
                 }
             }
-            &Typexpr::Array { index, elem } => {
-                validate.ty(index).map_err(|e| Error::InvalidIndex(id, e))?;
-                validate.ty(elem).map_err(|e| Error::InvalidElem(id, e))?;
-                if !matches!(index, Type::Fin { .. }) {
-                    return Err(Error::NotIndex(id));
+            Ty::Scope { id } => {
+                if id.block() >= def.blocks.len() {
+                    Err(Error::InvalidScope(t))
+                } else {
+                    Ok(())
                 }
             }
-            Typexpr::Tuple { members } => {
-                for (i, &member) in members.iter().enumerate() {
-                    validate
-                        .ty(member)
-                        .map_err(|e| Error::InvalidMember(id, id::member(i), e))?;
+            Ty::Ref { scope, inner } => {
+                if scope.ty() >= def.types.len() {
+                    Err(Error::InvalidScope(t))
+                } else if inner.ty() >= def.types.len() {
+                    Err(Error::InvalidInner(t))
+                } else if !matches!(def.types[scope.ty()], Ty::Scope { .. }) {
+                    Err(Error::NotScope(t))
+                } else {
+                    Ok(())
                 }
             }
-            Typexpr::Def { id, params } => todo!(),
-        }
+            Ty::Array { index, elem } => {
+                if index.ty() >= def.types.len() {
+                    Err(Error::InvalidIndex(t))
+                } else if elem.ty() >= def.types.len() {
+                    Err(Error::InvalidElem(t))
+                } else if !matches!(def.types[index.ty()], Ty::Fin { .. }) {
+                    Err(Error::NotIndex(t))
+                } else {
+                    Ok(())
+                }
+            }
+            Ty::Tuple { members } => members.iter().enumerate().try_for_each(|(i, member)| {
+                if member.ty() >= def.types.len() {
+                    Err(Error::InvalidMember(t, id::member(i)))
+                } else {
+                    Ok(())
+                }
+            }),
+        }?;
     }
 
     Ok(())
