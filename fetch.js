46df0b67bbb0e61e71b633038c6bb4e0079f49e3
Sam Estep <sam@samestep.com>
2023-07-18 09:41:46 -0400

    Use Node instead of curl (#58)

---
+++ fetch.js
@@ -0,0 +1,24 @@
+import { createWriteStream } from "fs";
+import https from "https";
+import { pipeline } from "stream/promises";
+import { fileURLToPath } from "url";
+
+// extract command line arguments
+const [fileUrl, outputPath] = process.argv.slice(2);
+
+https
+  .get(fileUrl, async (response) => {
+    try {
+      await pipeline(
+        response,
+        createWriteStream(fileURLToPath(new URL(outputPath, import.meta.url)))
+      );
+    } catch (error) {
+      console.error(`Error fetching resource: ${error}`);
+      process.exit(1);
+    }
+  })
+  .on("error", (error) => {
+    console.error(`Error: ${error}`);
+    process.exit(1);
+  });
