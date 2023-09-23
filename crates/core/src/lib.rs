a748516d6bddf6a4c54f2a40cd47a7aab0291e1a
Sam Estep <sam@samestep.com>
2023-09-23 01:28:04 -0400

    Undo core diff

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -67,6 +67,7 @@ pub trait Refs<'a> {
 }
 
 /// A node in a graph of functions.
+#[derive(Clone, Debug, Copy)]
 pub enum Node<'a, O, T: Refs<'a, Opaque = O>> {
     /// A function with an explicit body.
     Transparent {
