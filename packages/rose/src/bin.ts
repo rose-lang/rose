7d18dbb92ffb4fe756d0ea0060577e39586885a3
Sam Estep <sam@samestep.com>
2023-12-30 00:13:06 -0500

    Implement initial `rose build` command

--- packages/rose/src/bin.ts
+++ packages/rose/src/bin.ts
@@ -1,1 +1,42 @@
-console.log("Hello, world!");
+import * as fs from "fs/promises";
+import * as path from "path";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+const src = "src";
+const dist = "dist";
+
+const walk = async (dir: string) => {
+  const distDir = path.join(dist, dir);
+  await fs.mkdir(distDir, { recursive: true });
+  const srcDir = path.join(src, dir);
+  for (const child of await fs.readdir(srcDir)) {
+    const dirChild = path.join(dir, child);
+    const srcDirChild = path.join(src, dirChild);
+    if ((await fs.stat(srcDirChild)).isDirectory()) await walk(dirChild);
+    else {
+      const ext = path.extname(child);
+      if (ext === ".rose") {
+        const base = path.basename(child, ext);
+        const distDirChild = path.join(distDir, `${base}.js`);
+        const contents = await fs.readFile(srcDirChild, "utf8");
+        await fs.writeFile(
+          distDirChild,
+          `export default ${JSON.stringify(contents)};\n`,
+        );
+      }
+    }
+  }
+};
+
+yargs(hideBin(process.argv))
+  .command(
+    "build",
+    "build the current package",
+    () => {},
+    () => walk(""),
+  )
+  .demandCommand()
+  .strict()
+  .version("0.5.0") // TODO: why must this be manually specified?
+  .help().argv;
