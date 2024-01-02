e3662b56e87a2488389b47d2506ba0604adcceec
Sam Estep <sam@samestep.com>
2024-01-02 15:43:48 -0500

    Lex via Moo

---
+++ packages/rose/src/lex.test.ts
@@ -0,0 +1,46 @@
+import { expect, test } from "vitest";
+import { lexer } from "./lex.js";
+
+const tokens = (
+  source: string,
+): { type: string | undefined; text: string }[] => {
+  const lex = lexer();
+  lex.reset(source);
+  return [...lex].map(({ type, text }) => ({ type, text }));
+};
+
+test("comment", () => {
+  expect(
+    tokens(`# one comment
+
+# another comment`),
+  ).toEqual([
+    { type: "comment", text: "# one comment" },
+    { type: "space", text: "\n\n" },
+    { type: "comment", text: "# another comment" },
+  ]);
+});
+
+test("string", () => {
+  expect(tokens('"one string" "another string"')).toEqual([
+    { type: "str", text: '"one string"' },
+    { type: "space", text: " " },
+    { type: "str", text: '"another string"' },
+  ]);
+});
+
+test("integer", () => {
+  expect(tokens("42")).toEqual([{ type: "num", text: "42" }]);
+});
+
+test("float", () => {
+  expect(tokens("42.0")).toEqual([{ type: "num", text: "42.0" }]);
+});
+
+test("number followed by letter", () => {
+  expect(() => tokens("1a")).toThrow("invalid syntax");
+});
+
+test("letter followed by number", () => {
+  expect(tokens("a1")).toEqual([{ type: "id", text: "a1" }]);
+});
---
+++ packages/rose/src/lex.ts
@@ -0,0 +1,19 @@
+import moo from "moo";
+
+export const lexer = () =>
+  moo.compile({
+    op: /[^\s#\w"\(\)\[\]\{\},\.]+/,
+    space: { match: /\s+/, lineBreaks: true },
+    comment: /#.*?$/,
+    str: { match: /".*?"/, lineBreaks: true },
+    num: /\d+(?:\.\d+)?\b/,
+    id: /(?!\d)\w+/,
+    lparen: "(",
+    rparen: ")",
+    lbracket: "[",
+    rbracket: "]",
+    lbrace: "{",
+    rbrace: "}",
+    comma: ",",
+    dot: ".",
+  });
